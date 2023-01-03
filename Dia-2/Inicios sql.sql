7fe2f6866f2c32b16486b2d90151742af1d96312
JohanMejiaM <johanmejiam@hotmail.com>
2023-01-03 08:47:15 -0500

    Inicio de SQL

--- Dia-2/Inicios sql.sql
+++ Dia-2/Inicios sql.sql
@@ -1,14 +1,14 @@
 -- Asi se crea una base de datos
-CREATE DATABASE IF NOT EXISTS practicas;
+CREATE DATABASE practicas;
 
 USE practicas;
 
--- ahora procedemos a crear nuestra primera tabla
--- Tipos de datos : https://dev.mysql.com/doc/refman/8.0/en/data-types.html
+-- ahora procedemos a crear nuestra primeraa tabla
+-- Tipos de datos: https://dev.mysql.com/doc/refman/8.0/en/data-types.html
 -- AUTO_INCREMENT > solamente puede haber uno por tabla
 CREATE TABLE usuarios(
--- nombre datatype  [conf adicionales]
-	id      INT      AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    nombre  TEXT     NOT NULL,
-    dni     CHAR(8)  UNIQUE
+-- nombre datatype [con adicionales]
+	id INT auto_increment unique primary KEY,
+    nombre text not null,
+    dni char(8) unique
 );
